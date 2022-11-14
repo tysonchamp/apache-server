f1e780e8260d0d6347480733243815fd6358658a
Soumya Mondal <tysonchamp@users.noreply.github.com>
2022-11-14 17:25:35 +0530

    updated repo

--- webmin.sh
+++ webmin.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "deb http://download.webmin.com/download/repository sarge contrib" | cat >> /etc/apt/sources.list
-wget http://www.webmin.com/jcameron-key.asc
+echo "deb https://download.webmin.com/download/repository sarge contrib" | cat >> /etc/apt/sources.list
+wget https://download.webmin.com/jcameron-key.asc
 apt-key add jcameron-key.asc
 apt-get update && apt-get install webmin -y
